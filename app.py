b87ae9cd475623443c08754c0f840d26b0241593
yojhf <106582217+yojhf@users.noreply.github.com>
2022-06-24 17:32:42 +0900

    뉴스 검색 프로그램

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+from ast import keyword
+from flask import Flask, request, render_template
 import func
 import face
+import nvapi
 
 
-from flask import Flask, request, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template("hello.html")
+
+@app.route('/naver')
+def naver():
+    return render_template("naver.html")
     
 
 @app.route('/method', methods=['GET', 'POST'])
@@ -25,6 +33,25 @@ def method():
                 <img src='{url}' width='320'>
                 <img src='/static/img/face.png' width='320'>
                 '''
+
+@app.route('/newsapi', methods=['GET', 'POST'])
+def newsapi():
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        # 뉴스 검색어 받기
+        keyword = request.form["keyword"]
+        # 네이버 뉴스 코드 실행
+        news = nvapi.news(keyword)
+
+        return news 
+        
+        # return f'''
+        #         유저가 검색한 내용 <b>{keyword}의
+        #         뉴스 정보가 나와야 된다.<br>
                 
+        #         '''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import func
 import face
+import nvapi
+
 
 # func.face("이미지 주소")
 
 # 함수 호출
-face.face_func('https://th.bing.com/th/id/OIP.z7FZemly-c7BLFlHdSwLCwHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.25&pid=1.7')
+# face.face_func('https://th.bing.com/th/id/OIP.z7FZemly-c7BLFlHdSwLCwHaLH?w=182&h=273&c=7&r=0&o=5&dpr=1.25&pid=1.7')
+
+news = nvapi.news('불금')
+print(news) # nvapi.new에서 전달된 데이터를 출력
